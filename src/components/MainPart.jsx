0bbdf38df88b1d1e3e94a7338acd17dd37e6102f
Anna Wojtowicz <anna.agnieszka.wojtowicz@gmail.com>
2025-05-02 12:48:38 +0200

    feat: enhance MainPart component styles for improved readability and consistency

--- src/components/MainPart.jsx
+++ src/components/MainPart.jsx
@@ -4,7 +4,7 @@ import ArrowDown from "@mui/icons-material/ArrowDropDown";
 export default function MainPart() {
 
     let mainPartStyles = {
-        common: "text-[var(--anti-flash-white-500)]",
+        common: "text-[var(--anti-flash-white-500)] font-light",
         twoLinesStyle: "flex flex-col items-center justify-center",
         mainTemp: "text-6xl",
     };
@@ -14,15 +14,18 @@ export default function MainPart() {
         <div className="flex items-center justify-center">
             <div className="w-[290px] h-[290px] rounded-full border-ghost_white/30 bg-black/20 flex items-center justify-center">
                 <div className="w-[270px] h-[270px] rounded-full border-ghost_white/30 bg-black/40 flex items-center justify-center">
-                    <div className="flex flex-col items-center justify-center">
-                        <div className={`${mainPartStyles.common} ${mainPartStyles.tempIcon}`}><CloudIcon sx={{ fontSize: "3rem" }} /></div>
-                        <div className="flex items-center">
-                            <span className={`${mainPartStyles.twoLinesStyle}`}><div className={`${mainPartStyles.common}`}>wind</div><div className={`${mainPartStyles.common}`}>5m/s</div></span>
-                            <span className={`${mainPartStyles.twoLinesStyle}`}><div className={`${mainPartStyles.common} ${mainPartStyles.mainTemp}`}>23°C</div><div className={`${mainPartStyles.common}`}>feels like 20°C</div></span>
-                            <span className={`${mainPartStyles.twoLinesStyle}`}><div className={`${mainPartStyles.common}`}>percip.</div><div className={`${mainPartStyles.common}`}>0 mm</div></span>
+                    <div className="flex flex-col items-center justify-center gap-4">
+                        <div className={`${mainPartStyles.common} ${mainPartStyles.tempIcon}`}><CloudIcon sx={{ fontSize: "4rem" }} /></div>
+                        <div>
+                            <div className="flex items-center gap-5">
+                                <span className={`${mainPartStyles.twoLinesStyle} text-sm`}><div className={`${mainPartStyles.common}`}>wind</div><div className={`${mainPartStyles.common}`}>5m/s</div></span>
+                                <span className={`${mainPartStyles.twoLinesStyle}`}><div className={`${mainPartStyles.common} ${mainPartStyles.mainTemp}`}>23°C</div></span>
+                                <span className={`${mainPartStyles.twoLinesStyle} text-sm`}><div className={`${mainPartStyles.common}`}>percip.</div><div className={`${mainPartStyles.common}`}>0 mm</div></span>
+                            </div>
+                            <div className={`${mainPartStyles.common} text-center`}>feels like 20°C</div>
                         </div>
                         <div className={`${mainPartStyles.twoLinesStyle}`}>
-                            <div className={`${mainPartStyles.common}`}>more</div>
+                            <div className={`${mainPartStyles.common} text-sm`}>more</div>
                             <div className={`${mainPartStyles.common}`}><ArrowDown /></div>
                         </div>
                     </div>
