cdcdc34bb6b290ac80b2c2adcc83b913514e5edd
Anna Wojtowicz <anna.agnieszka.wojtowicz@gmail.com>
2025-05-03 16:41:45 +0200

    refactor: improve layout structure in App component for better readability

--- src/App.jsx
+++ src/App.jsx
@@ -29,19 +29,27 @@ function App() {
   return (
 
     <div className="bg-[url('../public/img/billy-huynh-v9bnfMCyKbg-unsplash.jpg')] min-h-screen w-full bg-cover bg-center bg-no-repeat flex items-center justify-center">
-      <div className="container max-w-[320px] h-[600px] mx-auto px-4 py-8 rounded-lg border-1 border-[#fafaff] bg-black/10">
-        <div className="flex flex-col max-w-[300px] justify-between">
-          {showSearch ?
-            <City executeHandleShowSearch={handleShowSearch} city={showSeachedCity} /> :
-            <Search value={showSeachedCity} onChange={handleShowSearchedCity} executeHandleShowSearch={handleShowSearch} />
-          }
-          {!showDetails ?
-            <MainPart executeHandleDetails={handleDetails} /> :
-            <Details executeHandleDetails={handleDetails} />
-          }
+      <div className="container w-[320px] h-[600px] mx-auto  pt-8 rounded-lg border-1 border-[#fafaff] bg-black/10 flex flex-col justify-between">
+        <div className="px-4">
+          <div className="flex flex-col max-w-[300px] justify-between">
+            {showSearch ?
+              <City executeHandleShowSearch={handleShowSearch} city={showSeachedCity} /> :
+              <Search value={showSeachedCity} onChange={handleShowSearchedCity} executeHandleShowSearch={handleShowSearch} />
+            }
+            {!showDetails ?
+              <MainPart executeHandleDetails={handleDetails} /> :
+              <Details executeHandleDetails={handleDetails} />
+            }
+          </div>
+        </div>
+
+        <div className="max-w-[320] h-[190px]  border-ghost_white/30 bg-black/20 flex items-center justify-center rounded-b-lg">
+          <div className="flex flex-col gap-4">
+            <div className="w-[318px] h-[70px]  border-ghost_white/30 bg-black/40 flex items-center justify-center">weather day + hour</div>
+            <div className="w-[318px] h-[70px]  border-ghost_white/30 bg-black/40 flex items-center justify-center">weather day + hour</div>
+          </div>
         </div>
       </div>
-
     </div>
 
   )
