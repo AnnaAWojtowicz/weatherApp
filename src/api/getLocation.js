c1378db8559ed0bafa644d164f00c7fd3409c663
Anna Wojtowicz <anna.agnieszka.wojtowicz@gmail.com>
2025-05-14 17:56:09 +0200

    Rename parameter in getLocation function from 'place' to 'searched' for clarity

--- src/api/getLocation.js
+++ src/api/getLocation.js
@@ -1,6 +1,6 @@
 
-export default async function getLocation({ place }) {
-    const getLocationApi = `https://nominatim.openstreetmap.org/search?q=${place}&format=json&limit=1`;
+export default async function getLocation({ searched }) {
+    const getLocationApi = `https://nominatim.openstreetmap.org/search?q=${searched}&format=json&limit=1`;
 
     try {
         const res = await fetch(getLocationApi);
