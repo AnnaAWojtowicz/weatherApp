edfe824c4ac822d3f9aa001516ea67ae11d2838e
Anna Wojtowicz <anna.agnieszka.wojtowicz@gmail.com>
2025-05-15 15:33:28 +0200

    Add dynamic weather icons to MainPart component based on weather conditions

--- src/components/MainPart.jsx
+++ src/components/MainPart.jsx
@@ -1,4 +1,11 @@
+import ThermometerIcon from "@mui/icons-material/Thermostat";
 import CloudIcon from "@mui/icons-material/Cloud";
+import SunIcon from "@mui/icons-material/Sunny";
+import RainIcon from "@mui/icons-material/Umbrella";
+import ThunderIcon from "@mui/icons-material/Thunderstorm";
+import FogIcon from "@mui/icons-material/Foggy";
+import SnowIcon from "@mui/icons-material/Snowing";
+import SleetIcon from "@mui/icons-material/CloudySnowing";
 import ArrowDown from "@mui/icons-material/ArrowDropDown";
 import getWeatherNow from "../api/getWeatherNow";
 
@@ -20,6 +27,42 @@ export default function MainPart({ executeHandleDetails, weatherData }) {
         wind_speed);
     const percip = Math.round(weatherData.properties.timeseries[0].data.next_1_hours.details.precipitation_amount)
 
+    let WeatherIcon = ThermometerIcon;
+    let weatherIconCode = weatherData.properties.timeseries[0].data.next_1_hours.summary.symbol_code;
+    if (weatherIconCode.includes("rain")) {
+        WeatherIcon = RainIcon;
+    } else if (weatherIconCode.includes("snow")) {
+        WeatherIcon = SnowIcon;
+    }
+    else if (weatherIconCode.includes("fog")) {
+        WeatherIcon = FogIcon;
+    }
+    else if (weatherIconCode.includes("cloud")) {
+        WeatherIcon = CloudIcon;
+    }
+    else if (weatherIconCode.includes("thunder")) {
+        WeatherIcon = ThunderIcon;
+    }
+    else if (weatherIconCode.includes("clear")) {
+        WeatherIcon = SunIcon;
+    }
+    else if (weatherIconCode.includes("partly")) {
+        WeatherIcon = CloudIcon;
+    }
+    else if (weatherIconCode.includes("overcast")) {
+        WeatherIcon = CloudIcon;
+    }
+    else if (weatherIconCode.includes("sleet")) {
+        WeatherIcon = SleetIcon;
+    }
+    else if (weatherIconCode.includes("showers")) {
+        WeatherIcon = RainIcon;
+    }
+    else if (weatherIconCode.includes("fair")) {
+        WeatherIcon = SunIcon;
+    }
+
+
 
     return (
 
@@ -27,7 +70,7 @@ export default function MainPart({ executeHandleDetails, weatherData }) {
             <div className="w-[290px] h-[290px] rounded-full border-ghost_white/30 bg-black/20 flex items-center justify-center">
                 <div className="w-[270px] h-[270px] rounded-full border-ghost_white/30 bg-black/40 flex items-center justify-center">
                     <div className="flex flex-col items-center justify-center gap-4">
-                        <div className={`${mainPartStyles.common} ${mainPartStyles.tempIcon}`}><CloudIcon sx={{ fontSize: "4rem" }} /></div>
+                        <div className={`${mainPartStyles.common} ${mainPartStyles.tempIcon}`}><WeatherIcon sx={{ fontSize: "4rem" }} /></div>
                         <div>
                             <div className="flex items-center gap-5">
                                 <span className={`${mainPartStyles.twoLinesStyle} text-sm`}><div className={`${mainPartStyles.common}`}>wind</div><div className={`${mainPartStyles.common}`}>{wind} m/s</div></span>
